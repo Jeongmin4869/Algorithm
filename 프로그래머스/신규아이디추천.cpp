f57cbe0f6f4a48375da1e2249f98fb050c04df87
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-03 22:53:49 +0900

    진행중

--- 프로그래머스/신규아이디추천.cpp
+++ 프로그래머스/신규아이디추천.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 
+
 using namespace std;
 
 string solution(string new_id) {
@@ -14,9 +15,25 @@ string solution(string new_id) {
 	//2단계
 
 	//3단계
-	if (new_id[0] == '.') new_id.erase(0);
-	if (new_id[new_id.end() - 1] == '.') new_id.erase(new_id.end() - 1);
 
+	//4단계
+	if (new_id[0] == '.') new_id.erase(0);
+	if (new_id[new_id.length() - 1] == '.') new_id.erase(new_id.length() - 1);
+
+	//5단계
+	if (new_id.length() == 0) new_id = "a";
+
+	//6단계
+	if (new_id.length() >= 16) new_id = new_id.substr(0, 15);
+	if (new_id[new_id.length() - 1] == '.') new_id.erase(new_id.length() - 1);
+
+	//7단계
+	if (new_id.length() <= 2) {
+		string last = new_id.substr(new_id.length(), new_id.length());
+		while (new_id.length() == 3) {
+			new_id.append(last);
+		}
+	}
 
 	return new_id;
 }
