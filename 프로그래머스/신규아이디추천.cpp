200ece406a0d85d81a7b1c31e245dc329ab159e7
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-04 22:57:38 +0900

    해결중

--- 프로그래머스/신규아이디추천.cpp
+++ 프로그래머스/신규아이디추천.cpp
@@ -8,13 +8,35 @@ string solution(string new_id) {
 	string answer = "";
 
 	//1단계
-	for (int i : new_id) {
+	for (int i = 0; i < new_id.length(); i++) {
 		new_id[i] = tolower(new_id[i]);
 	}
 
+
 	//2단계
 
+	for (int i = 0; i < new_id.length(); i++) {
+		if ((new_id[i] == '-' || new_id[i] == '_' || new_id[i] == '.') ||
+			(new_id[i] >= 'a' && new_id[i] <= 'z') ||
+			(new_id[i] >= '1' && new_id[i] <= '9')) {//숫자
+			printf("d", new_id.c_str());
+			continue;
+		}
+		else {
+		}
+
+	}
+
+
+
 	//3단계
+	for (int i = 0; i < new_id.length() - 1; i++) {
+		if (new_id[i] == new_id[i + 1] && new_id[i] == '.') {
+			new_id.erase(i + 1);
+			i--;
+		}
+	}
+	//printf("%s", new_id.c_str());
 
 	//4단계
 	if (new_id[0] == '.') new_id.erase(0);
