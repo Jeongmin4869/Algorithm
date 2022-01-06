df9b3c52472507a5044ebad130b73abf6b72cdba
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-07 01:34:29 +0900

    구현중

--- 프로그래머스/숫자 문자열과 영단어.cpp
+++ 프로그래머스/숫자 문자열과 영단어.cpp
@@ -1,27 +1,32 @@
 ﻿#include <string>
 #include <vector>
 #include <unordered_map>
+#include <cstring>
 using namespace std;
 
 int solution(string s) {
 	int answer = 0;
-	unordered_map<string, int> hash;
-	hash["one"] = 1;
-	hash["two"] = 2;
-	hash["three"] = 3;
-	hash["four"] = 4;
-	hash["five"] = 5;
-	hash["six"] = 6;
-	hash["seven"] = 7;
-	hash["eight"] = 8;
-	hash["nine"] = 9;
+	unordered_map<int, string> hash;
+	hash[1] = "one";
+	hash[2] = "two";
+	hash[3] = "three";
+	hash[4] = "four";
+	hash[5] = "five";
+	hash[6] = "six";
+	hash[7] = "seven";
+	hash[8] = "eight";
+	hash[9] = "nine";
 
-	for (int i = 0; i < s.length(); i++) {
-		if (s[i] >= '0' && s[i] <= '9') // 숫자일때
-
-			answer += s[i] % '0';
-		//if(hash[substr(i,3)])
+	for (int i = 1; i < 10; i++) {
+		/*
+		if(s[i] >= '0' && s[i] <= '9') // 숫자일때
+			answer += s[i]%'0';
+		*/
+		if (strchr(s, hash[i]))
+			printf("a");
+		//strchr("-_.", ch)
 	}
 
+	//string에 '0' 곱해서 답하기
 	return answer;
 }
