efddac160ba8d67138ee5ec5686c2c699ed9cb54
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-08 00:44:51 +0900

    C++

--- 프로그래머스/숫자 문자열과 영단어.cpp
+++ 프로그래머스/숫자 문자열과 영단어.cpp
@@ -3,18 +3,16 @@
 using namespace std;
 
 int solution(string s) {
-	//oneoneoneone => 1oneoneone
 	int answer = 0;
-	string arr[] = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
-	//hash key가 int일바에 차라리 배열을... 
-	printf("%d", sizeof(arr) / sizeof(arr[0]));
-	for (int i = 0; i < sizeof(string); i++) {
-		//if(s.find(arr[i])!= string::npos){
-		   //s = s.replace(s.find(hash[i]), hash[i].length(),to_string(i));
+	string arr[] = { "zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+	int len = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
 
-		//}
-
+	while (i < len) {
+		if (s.find(arr[i]) != string::npos)
+			s = s.replace(s.find(arr[i]), arr[i].length(), to_string(i));
+		else i++;
 	}
 
-	return answer;
+	return stoi(s);
 }
