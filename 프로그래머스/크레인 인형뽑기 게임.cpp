e7d0e6551438522da4205676ea0127422aa97e27
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-11 23:02:19 +0900

    구현중

---
+++ 프로그래머스/크레인 인형뽑기 게임.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+	int answer = 0;
+	for (int i = 0; i < board.size(); i++) {
+		for (int j = 0; j < board[i].size(); j++) {
+
+		}
+	}
+	//다 구하고 터트리기?
+
+	return answer; // 사라진 인형의 갯수
+}
