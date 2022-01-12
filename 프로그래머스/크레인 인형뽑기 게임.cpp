2e0471855968922588d1398d06ad7375bfdcd207
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-12 22:41:38 +0900

    C++

--- 프로그래머스/크레인 인형뽑기 게임.cpp
+++ 프로그래머스/크레인 인형뽑기 게임.cpp
@@ -4,13 +4,23 @@
 using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
-	int answer = 0;
-	for (int i = 0; i < board.size(); i++) {
-		for (int j = 0; j < board[i].size(); j++) {
+	int count = 0;
+	vector<int> box = {};
+	int num = 10;
 
+	for (int i = 0; i < moves.size(); i++) {
+		for (int j = 0; j < board.size(); j++) {
+			if (board[j][moves[i] - 1] != 0) {
+				num = moves[i] - 1;
+				if (!box.empty() && box.back() == board[j][num]) {
+					box.pop_back();
+					count += 2;
+				}
+				else box.push_back(board[j][num]);
+				board[j][num] = 0;
+				break;
+			}
 		}
 	}
-	//다 구하고 터트리기?
-
-	return answer; // 사라진 인형의 갯수
+	return count; // 사라진 인형의 갯수
 }
