505c52cdb0b6373ede1c054d4422d3a4573b7f50
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-13 22:52:12 +0900

    구현중

--- 프로그래머스/실패율.cpp
+++ 프로그래머스/실패율.cpp
@@ -1,9 +1,33 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(int N, vector<int> stages) {
 	vector<int> answer;
+	vector<int> p;
+	vector<float> t;
+	int person = stages.size();
+
+	p.assign(N, 0);
+	for (int i = 0; i < stages.size(); i++) {
+		if (stages[i] <= N)
+			p[stages[i] - 1] ++;
+	}
+
+	for (int i = 0; i < p.size(); i++) {
+		t.push_back((float)p[i] / person);
+		person -= p[i];
+	}
+
+	for (int i = 0; i < t.size(); i++) {
+		int max_index = max_element(t.begin(), t.end()) - t.begin();
+		printf("%d ", max_index);
+		answer.push_back(max_index + 1);
+		t[max_index] = -1;
+	}
+
+
 	return answer;
 }
