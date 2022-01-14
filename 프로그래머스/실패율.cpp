4c2106ff6105947b1a2d28e9d5c489cc8158e1e1
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-14 22:24:46 +0900

    C++

--- 프로그래머스/실패율.cpp
+++ 프로그래머스/실패율.cpp
@@ -6,28 +6,30 @@ using namespace std;
 
 vector<int> solution(int N, vector<int> stages) {
 	vector<int> answer;
-	vector<int> p;
-	vector<float> t;
+	vector<int> stay;
+	vector<float> fRate;
 	int person = stages.size();
 
-	p.assign(N, 0);
+	stay.assign(N, 0);
 	for (int i = 0; i < stages.size(); i++) {
 		if (stages[i] <= N)
-			p[stages[i] - 1] ++;
+			stay[stages[i] - 1] ++;
 	}
 
-	for (int i = 0; i < p.size(); i++) {
-		t.push_back((float)p[i] / person);
-		person -= p[i];
+	for (int i = 0; i < stay.size(); i++) {
+		if (person == 0) {
+			fRate.push_back(0.0);
+			continue;
+		}
+		fRate.push_back((float)stay[i] / person);
+		person -= stay[i];
 	}
 
-	for (int i = 0; i < t.size(); i++) {
-		int max_index = max_element(t.begin(), t.end()) - t.begin();
-		printf("%d ", max_index);
+	for (int i = 0; i < fRate.size(); i++) {
+		int max_index = max_element(fRate.begin(), fRate.end()) - fRate.begin();
 		answer.push_back(max_index + 1);
-		t[max_index] = -1;
+		fRate[max_index] = -1;
 	}
-
 
 	return answer;
 }
