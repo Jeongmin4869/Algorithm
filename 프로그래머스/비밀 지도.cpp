e2a0a8096b689b82c19e366fa913edc859ef0b0c
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-15 22:46:43 +0900

    C++

--- 프로그래머스/비밀 지도.cpp
+++ 프로그래머스/비밀 지도.cpp
@@ -1,9 +1,21 @@
 #include <string>
 #include <vector>
-
+#include <bitset>
 using namespace std;
+
+string to_binary(int num, int n) {
+	string s = "";
+	for (int i = 0; i < n; i++) {
+		num % 2 == 0 ? s.insert(0, " ") : s.insert(0, "#");
+		num >>= 1; // /=2
+	}
+	return s;
+}
 
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
 	vector<string> answer;
+	for (int i = 0; i < n; i++) {
+		answer.push_back(to_binary(arr1[i] | arr2[i], n));
+	}
 	return answer;
 }
