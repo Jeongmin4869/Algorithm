f0ec9ee8954b2b31529e8df20c459786782d6667
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-16 22:34:07 +0900

    구현중

--- 프로그래머스/다트게임.cpp
+++ 프로그래머스/다트게임.cpp
@@ -1,8 +1,32 @@
 #include <string>
+#include <cmath>
 
 using namespace std;
 
 int solution(string dartResult) {
 	int answer = 0;
+	int i = 0;
+	while (i + 2 < dartResult.length()) {
+		int num = dartResult.at(i) - '0';
+		char char1 = dartResult.at(i + 1);
+		if (char1 == 'S') {
+			answer += num;
+		}
+		else if (char1 == 'D') {
+			answer += pow(num, 2);
+		}
+		else answer += pow(num, 3);
+
+		char char2 = dartResult.at(i + 2);
+
+		if (char2 >= '0' &&char2 <= '9') {
+			i += 2;
+			continue;
+		}
+		else {
+			//if(char2 == '*')
+		}
+		i += 3;
+	}
 	return answer;
 }
