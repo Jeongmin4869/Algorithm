fbfafca993f511c68ac50a1e58041ccfbc45b371
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-19 22:35:47 +0900

    C++
    변수명 num 에서 score로 변경

--- 프로그래머스/다트게임.cpp
+++ 프로그래머스/다트게임.cpp
@@ -6,39 +6,39 @@ using namespace std;
 int solution(string dartResult) {
 	int answer = 0;
 	int i = 0;
-	vector<int> num(3, 0);
+	vector<int> score(3, 0);
 	char word;
 
 	for (int j = 0; j < 3; j++) {
-		num[j] = dartResult[i] - '0';
+		score[j] = dartResult[i] - '0';
 		if (dartResult[i + 1] == '0') {
-			num[j] = 10;
+			score[j] = 10;
 			i++;
 		}
 
 		word = dartResult[++i];
 		if (word == 'D') {
-			num[j] = pow(num[j], 2);
+			score[j] = pow(score[j], 2);
 		}
 		else if (word == 'T') {
-			num[j] = pow(num[j], 3);
+			score[j] = pow(score[j], 3);
 		}
 
 		if (dartResult[++i] != NULL) {
 			word = dartResult[i];
 			if (word == '*') {
-				num[j] = num[j] * 2;
+				score[j] = score[j] * 2;
 				if (i != 0) {
-					num[j - 1] = num[j - 1] * 2;
+					score[j - 1] = score[j - 1] * 2;
 				}
 			}
 			else if (word == '#') {
-				num[j] = num[j] * -1;
+				score[j] = score[j] * -1;
 			}
 			else continue;
 		}
 		i++;
 	}
 
-	return answer = num[0] + num[1] + num[2];
+	return answer = score[0] + score[1] + score[2];
 }
