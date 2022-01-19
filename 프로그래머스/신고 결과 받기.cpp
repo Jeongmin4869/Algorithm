95c179de8284e466180375f1a020bbf63f9fa9e0
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-19 22:21:25 +0900

    구현중

--- 프로그래머스/신고 결과 받기.cpp
+++ 프로그래머스/신고 결과 받기.cpp
@@ -1,9 +1,22 @@
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
 	vector<int> answer;
+	unordered_map<string, int> hash;
+	for (int i = 0; i < id_list.size(); i++) {
+		hash[id_list[i]];
+	}
+	for (int i = 0; i < report.size(); i++) {
+		hash[report[i]] = hash[report[i]] + 1;
+	}
+	for (int i = 0; i < id_list.size(); i++) {
+		answer.push_back(hash[id_list[i]]);
+	}
+
+
 	return answer;
 }
