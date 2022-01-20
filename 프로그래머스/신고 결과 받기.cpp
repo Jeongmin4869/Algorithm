5da92d884bbfe505ade1ce9d8cb6d3f5d4da306d
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-20 22:56:57 +0900

    구현중
    istringstream

--- 프로그래머스/신고 결과 받기.cpp
+++ 프로그래머스/신고 결과 받기.cpp
@@ -1,21 +1,26 @@
-#include <string>
+﻿#include <string>
 #include <vector>
 #include <unordered_map>
+#include <algorithm> //unique
+#include <sstream> // istringstream
 
 using namespace std;
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
 	vector<int> answer;
+	vector<int> ban;
+	vector<string> data;
 	unordered_map<string, int> hash;
-	for (int i = 0; i < id_list.size(); i++) {
-		hash[id_list[i]];
-	}
+
+	//중복값 제거
+	sort(report.begin(), report.end());
+	report.erase(unique(report.begin(), report.end()), report.end());
+
 	for (int i = 0; i < report.size(); i++) {
-		hash[report[i]] = hash[report[i]] + 1;
+		istringstream ss(report[i]);
 	}
-	for (int i = 0; i < id_list.size(); i++) {
-		answer.push_back(hash[id_list[i]]);
-	}
+
+	//result : 각 이용자 당 받는 처리결과메일 갯수 ...  정지되면 0
 
 
 	return answer;
