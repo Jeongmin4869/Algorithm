b5a29ecc9840c5b746c907092d15fcc38ca39a49
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-21 22:47:17 +0900

    구현중

--- 프로그래머스/신고 결과 받기.cpp
+++ 프로그래머스/신고 결과 받기.cpp
@@ -7,21 +7,33 @@
 using namespace std;
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
-	vector<int> answer;
-	vector<int> ban;
-	vector<string> data;
-	unordered_map<string, int> hash;
-
+	string name;
+	string report_name;
+	int idx = 0;
+	vector<vector<string>> user(id_list.size(), vector<string>());
+	vector<int> report_count(id_list.size(), 0); //신고당한 횟수
+	vector<int> answer(id_list.size(), 0);
+	vector<string> report_user;
 	//중복값 제거
 	sort(report.begin(), report.end());
 	report.erase(unique(report.begin(), report.end()), report.end());
 
 	for (int i = 0; i < report.size(); i++) {
 		istringstream ss(report[i]);
+		ss >> name;
+		idx = find(id_list.begin(), id_list.end(), name) - id_list.begin(); // index
+		ss >> report_name;
+		user[idx].push_back(report_name);
+		idx = find(id_list.begin(), id_list.end(), report_name) - id_list.begin(); // index
+		report_count[i]++;
 	}
 
-	//result : 각 이용자 당 받는 처리결과메일 갯수 ...  정지되면 0
-
+	for (int i = 0; i < report_count.size(); i++) {
+		if (report_count[i] >= k) {
+			//정지된 아이디
+			report_user.push_back(id_list[i]);
+		}
+	}
 
 	return answer;
 }
