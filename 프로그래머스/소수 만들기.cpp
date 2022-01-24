41269171b527935c83f7acccb6d251f91cddad06
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-24 21:00:57 +0900

    구현중

---
+++ 프로그래머스/소수 만들기.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> nums) {
+	int answer = -1;
+
+	for (int i = 0; i < nums.size() - 2; i++) {
+		for (int j = i; j < nums.size() - 1; j++) {
+			for (int z = j; j < nums.size(); j++) {
+
+			}
+		}
+	}
+
+	return answer;
+}
