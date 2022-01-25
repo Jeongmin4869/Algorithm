b541f016bc62fa93353c3363829e49c348968168
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-25 22:12:16 +0900

    C++

--- 프로그래머스/소수 만들기.cpp
+++ 프로그래머스/소수 만들기.cpp
@@ -1,14 +1,22 @@
 #include <vector>
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+bool sosu(int num) {
+	if (num < 2) return false;
+	int a = (int)sqrt(num);
+	for (int i = 2; i <= a; i++) if (num % i == 0) return false;
+	return true;
+}
+
 int solution(vector<int> nums) {
-	int answer = -1;
+	int answer = 0;
 
 	for (int i = 0; i < nums.size() - 2; i++) {
-		for (int j = i; j < nums.size() - 1; j++) {
-			for (int z = j; j < nums.size(); j++) {
-
+		for (int j = i + 1; j < nums.size() - 1; j++) {
+			for (int z = j + 1; z < nums.size(); z++) {
+				if (sosu(nums[i] + nums[j] + nums[z])) answer++;
 			}
 		}
 	}
