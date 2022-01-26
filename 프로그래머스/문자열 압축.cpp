4084f6772ee17ffdb0b2adee1c8f7d109197ed15
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-26 22:47:20 +0900

    구현중

--- 프로그래머스/문자열 압축.cpp
+++ 프로그래머스/문자열 압축.cpp
@@ -1,16 +1,24 @@
 #include <string>
 #include <vector>
-
 using namespace std;
 
 int solution(string s) {
 	int answer = s.length();
 	int len = 0;
+	int count = 0;
 	string a, b_s, f_s;
 
 	for (int i = 2; i < s.length() / 2; i++) {
-		a = s;
+
+		for (int j = 0; j < s.length(); j += i) {
+			f_s = s.substr(j, i);
+			//substr(3, 5)
+			b_s = f_s;
+		}
+
 		if (a.length() < answer) answer = a.length();
+
+
 	}
 
 	return answer;
