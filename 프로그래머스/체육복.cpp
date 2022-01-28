0ff474d3835853939fe7af69c80112417250601f
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-28 22:53:55 +0900

    구현중

--- 프로그래머스/체육복.cpp
+++ 프로그래머스/체육복.cpp
@@ -5,22 +5,15 @@
 using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
-	int answer = reserve.size();
-	int num;
-	for (int i = 0; i < reserve.size(); i++) {
-		num = i + 1;
-		auto it1 = find(lost.begin(), lost.end(), num - 1);
-		auto it2 = find(lost.begin(), lost.end(), num + 1);
-		if (it1 != lost.end()) {
-			answer++;
-			lost.erase(remove(lost.begin(), lost.end(), num - 1), lost.end());
-		}
-		else if (it2 != lost.end()) {
-			answer++;
-			lost.erase(remove(lost.begin(), lost.end(), num + 1), lost.end());
-		}
-	}
+	int answer = n - lost.size();
+	int j = 0;
+	for (int i = 0; i < lost.size(); i++) {
+		if ((lost[i] == reserve[j]) ||
+			(lost[i] - 1 == reserve[j]) ||
+			(lost[i] + 1 == reserve[j])) j++, answer++;
 
+		if (j == reserve.size()) break;
+	}
 
 	return answer;
 }
