34a3d0ff0bcb0f2af4fc6ba5619c3e801a41a562
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-30 00:48:28 +0900

    C++

--- 프로그래머스/체육복.cpp
+++ 프로그래머스/체육복.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -8,15 +7,8 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
 	int answer = 0;
 	vector<int> students(n, 1);
 
-	for (int i = 0; i < lost.size(); i++) {
-		students[lost[i] - 1]--;
-	}
-
-	for (int i = 0; i < reserve.size(); i++) {
-		students[reserve[i] - 1]++;
-	}
-
-	printf("%d ", students[2]);
+	for (int i : lost) students[i - 1]--;
+	for (int i : reserve) students[i - 1]++;
 
 	for (int i = 0; i < n; i++) {
 		if (students[i] == 0) {
