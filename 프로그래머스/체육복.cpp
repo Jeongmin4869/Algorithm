e56a90cc1c7ad9a109f2b45455d2b187a338709c
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-01-29 23:35:55 +0900

    구현중

--- 프로그래머스/체육복.cpp
+++ 프로그래머스/체육복.cpp
@@ -5,18 +5,23 @@
 using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
-	int answer = n - lost.size();
-	int j, k = 0;
+	int answer = 0;
+	vector<int> students(n, 1);
+
 	for (int i = 0; i < lost.size(); i++) {
-		for (j = k; j < reserve.size(); j++) {
-			if ((lost[i] == reserve[j]) ||
-				(lost[i] - 1 == reserve[j]) ||
-				(lost[i] + 1 == reserve[j])) {
-				k = j + 1;
-				answer++;
-				break;
-			}
+		students[lost[i]]--;
+	}
+
+	for (int i = 0; i < reserve.size(); i++) {
+		students[reserve[i]]++;
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (students[i] == 0) {
+			if (i > 0 && students[i - 1] > 1) { answer++; continue; }
+			else if (i < n && students[i + 1] > 1) { students[i + 1]--; answer++; continue; }
 		}
+		else answer++;
 	}
 
 	return answer;
