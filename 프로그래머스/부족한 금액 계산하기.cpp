3df4c724f6cb3dfb7f255da87a33c8ad3b90676c
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-02-06 20:28:19 +0900

    C++

--- 프로그래머스/부족한 금액 계산하기.cpp
+++ 프로그래머스/부족한 금액 계산하기.cpp
@@ -2,9 +2,9 @@ using namespace std;
 
 long long solution(int price, int money, int count)
 {
-	long answer = 0;
+	long long answer = 0;
 	for (int i = count; i > 0; i--) {
-		answer += (long)(price * i);
+		answer += (price * i);
 	}
 	return answer - money > 0 ? answer - money : 0;
 }
