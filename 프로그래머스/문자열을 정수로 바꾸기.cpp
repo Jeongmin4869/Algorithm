fda6acb31a341d36e2601577465d75ff2fe34ef8
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-02-15 22:14:07 +0900

    C++

---
+++ 프로그래머스/문자열을 정수로 바꾸기.cpp
@@ -0,0 +1,14 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+
+int solution(string s) {
+	int answer = 1;
+	if (s[0] == '-') {
+		answer = -1;
+		s = s.substr(1, s.length() - 1);
+	}
+	answer *= stoi(s);
+	//answer = stoi(s)만 해주어도 된다. 부호도 저절로 바꿔준다.
+	return answer;
+}
