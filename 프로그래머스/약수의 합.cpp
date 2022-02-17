55b505748ad2d0647e82760bc4fa86eaf82d751d
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-02-17 22:04:35 +0900

    C++

--- 프로그래머스/약수의 합.cpp
+++ 프로그래머스/약수의 합.cpp
@@ -6,12 +6,9 @@ using namespace std;
 int solution(int n) {
 	int answer = 0;
 
-	if (sqrt(n) <= 1) {
-		for (int i = 1; i <= n; i++) answer += i;
-		return answer;
-	}
+	if (pow((int)sqrt(n), 2) == (float)n) answer += (int)sqrt(n);
 
-	for (int i = 1; i <= sqrt(n); i++) {
+	for (int i = 1; i < sqrt(n); i++) {
 		if (n%i == 0) {
 			answer += i;
 			answer += n / i;
