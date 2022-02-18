657621329163e6022b21fe17f6d10c2c7c3e4d75
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-02-19 01:32:18 +0900

    C++

---
+++ 프로그래머스/정수 내림차순으로 배치하기.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+long long solution(long long n) {
+	long long answer = 0;
+	vector<long long> num;
+	string s = to_string(n);
+	for (int i = 0; i < s.length(); i++) {
+		num.push_back(s[i] - '0');
+	}
+	int size = num.size();
+	sort(num.begin(), num.end(), greater<>());
+	for (int i = 0; i < size; i++) {
+		answer += num[i] * pow(10, size - 1 - i);
+	}
+	//스트링 정렬
+	return answer;
+}
