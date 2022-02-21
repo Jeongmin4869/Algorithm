e39e895f8e731e03f6811d778bc2aa36c06311ff
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-02-21 21:27:57 +0900

    C++

--- 프로그래머스/최대공약수와 최소공배수.cpp
+++ 프로그래머스/최대공약수와 최소공배수.cpp
@@ -5,19 +5,13 @@ using namespace std;
 
 vector<int> solution(int n, int m) {
 	vector<int> answer;
-	int num = 2;
 	for (int i = min(n, m); i >= 1; i--) {
 		if (n%i == 0 && m%i == 0) {
 			answer.push_back(i);
+			answer.push_back(n / answer[0] * m / answer[0] * answer[0]);
 			break;
 		}
 	}
 
-	for (int i = min(n, m); i <= n * m; i = min(n, m) * num++) {
-		if (i%max(n, m) == 0) {
-			answer.push_back(i);
-			break;
-		}
-	}
 	return answer;
 }
