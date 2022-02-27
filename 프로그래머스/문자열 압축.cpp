f0a8cb2c649d25117988f847317cbc5414c75d1b
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-02-27 22:18:08 +0900

    C++

--- 프로그래머스/문자열 압축.cpp
+++ 프로그래머스/문자열 압축.cpp
@@ -4,22 +4,30 @@ using namespace std;
 
 int solution(string s) {
 	int answer = s.length();
-	int len = 0;
-	bool first = false;
+	int len = 0, count = 1;
 	string a, before;
 
 	for (int i = 1; i <= s.length() / 2; i++) {
 		len = 0;
 		for (int j = 0; j < s.length(); j += i) {
 			a = s.substr(j, i);
-			len += i;
+			len += a.length();
 			if (before == a) {
-				printf("%d ", i);
-				len -= i;
-				if (!first) len++, first = true;
+				len -= a.length();
+				count++;
 			}
-			else first = false;
+			else if (count > 1) {
+				string count_s = to_string(count);
+				len += count_s.length();
+				count = 1;
+			}
 			before = a;
+		}
+
+		if (count > 1) {
+			string count_s = to_string(count);
+			len += count_s.length();
+			count = 1;
 		}
 		if (answer > len) answer = len;
 	}
