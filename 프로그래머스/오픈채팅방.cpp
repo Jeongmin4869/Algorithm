a9ace32ee334e16b7237d15f0bd7ec552af687ec
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-02-28 22:09:44 +0900

    구현중

--- 프로그래머스/오픈채팅방.cpp
+++ 프로그래머스/오픈채팅방.cpp
@@ -6,13 +6,15 @@ using namespace std;
 
 vector<string> solution(vector<string> record) {
 	vector<string> answer;
-	string s;
 	stringstream stream1;
-	for (int i = 1; i < record.length(); i++) {
+	for (int i = 1; i < record.size(); i++) {
 		stream1.str(record[i]);
-		string id, k;
-		str >> k;
-		str >> id;
+		string id, state, s;
+		stream1 >> state >> id;
+		stream1 >> id;
+		s += id + "님이 ";
+		state == "Enter" ? s += "들어왔습니다." : s += "나갔습니다.";
+		answer.push_back(s);
 	}
 	return answer;
 }
