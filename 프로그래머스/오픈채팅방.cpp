6f5cd086cf06c424caff7a45eb4417cfc47fc5f1
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-01 01:47:10 +0900

    C++

--- 프로그래머스/오픈채팅방.cpp
+++ 프로그래머스/오픈채팅방.cpp
@@ -1,20 +1,38 @@
 ﻿#include <string>
 #include <vector>
 #include <sstream>
-
+#include <unordered_map>
+#include <iostream>
 using namespace std;
 
 vector<string> solution(vector<string> record) {
+	//map, vector
+	unordered_map<string, string> map;
+	vector<vector<string>> user;
 	vector<string> answer;
-	stringstream stream1;
-	for (int i = 1; i < record.size(); i++) {
+	for (int i = 0; i < record.size(); i++) {
+		vector<string> vec;
+		stringstream stream1;
 		stream1.str(record[i]);
-		string id, state, s;
-		stream1 >> state >> id;
-		stream1 >> id;
-		s += id + "님이 ";
-		state == "Enter" ? s += "들어왔습니다." : s += "나갔습니다.";
+		string id, state, name;
+		stream1 >> state >> id >> name;
+
+		if (state != "Change") {
+			vec.push_back(state);
+			vec.push_back(id);
+			user.push_back(vec);
+			cout << state << " ";
+		}
+		if (state != "Leave") map[id] = name;
+	}
+	cout << endl;
+	for (auto a : user) {
+		string s;
+		s += map[a[1]] + "님이 ";
+		a[0] == "Enter" ? s += "들어왔습니다." : s += "나갔습니다.";
 		answer.push_back(s);
 	}
+
+
 	return answer;
 }
