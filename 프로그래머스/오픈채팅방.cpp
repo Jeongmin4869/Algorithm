bc0bcc61dc81626e802fe7c0a53f165e21128e9f
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-02-28 21:29:37 +0900

    구현중

---
+++ 프로그래머스/오픈채팅방.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+vector<string> solution(vector<string> record) {
+	vector<string> answer;
+	string s;
+	stringstream stream1;
+	for (int i = 1; i < record.length(); i++) {
+		stream1.str(record[i]);
+		string id, k;
+		str >> k;
+		str >> id;
+	}
+	return answer;
+}
