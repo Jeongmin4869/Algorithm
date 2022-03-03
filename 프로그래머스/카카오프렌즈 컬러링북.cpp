04468aeffa4bd9c6a6bf7858b93051de0c4f8181
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-03 21:57:57 +0900

    구현중
    깊이탐색을 이용해 문제를 풀어야하기 때문에
    깊이우선탐색(DFS)과 너비우선탐색(BFS)을 공부중!

--- 프로그래머스/카카오프렌즈 컬러링북.cpp
+++ 프로그래머스/카카오프렌즈 컬러링북.cpp
@@ -2,16 +2,23 @@
 
 using namespace std;
 
-//깊이탐색...
-void DFS(int x, int y) {
+bool visit[100][100];
+int num = 0, cnt = 0, N, M;
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1,0,0 };
+
+//깊이탐색
+//DFS : 깊이우선탐색
+//BFS : 너비우선탐색, 큐
+void DFS(int x, int y, vector<vector<int>> picture) {
 	visit[x][y] = true;
 	cnt++;
 	for (int i = 0; i < 4; i++) {
 		int nx = x + dx[i];
 		int ny = y + dy[i];
 		if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-		if (arr[nx][ny] == 0 && !visit[nx][ny]) {
-			DFS(nx, ny);
+		if (picture[nx][ny] == 0 && !visit[nx][ny]) {
+			DFS(nx, ny, picture);
 		}
 	}
 }
@@ -20,6 +27,15 @@ void DFS(int x, int y) {
 vector<int> solution(int m, int n, vector<vector<int>> picture) {
 	int number_of_area = 0;
 	int max_size_of_one_area = 0;
+
+	visit[100][100] = { false, };
+	num = 0, cnt = 0;
+	//dx[4] = { 0, 0, 1, -1 };
+	//dy[4] = { 1, -1, 0, 0 };
+	N = n;
+	M = m;
+
+
 	//우측과 하단을 계속 스캔??
 
 
