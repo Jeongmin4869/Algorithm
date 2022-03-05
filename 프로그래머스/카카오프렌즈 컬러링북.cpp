28367eb50733987dcf74e0101701719849477013
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-05 20:07:22 +0900

    구현중

--- 프로그래머스/카카오프렌즈 컬러링북.cpp
+++ 프로그래머스/카카오프렌즈 컬러링북.cpp
@@ -1,5 +1,5 @@
 ﻿#include <vector>
-
+#include <iostream>
 using namespace std;
 
 bool visit[100][100];
@@ -23,20 +23,27 @@ void DFS(int x, int y, vector<vector<int>> picture, int num) {
 	}
 }
 
-// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 vector<int> solution(int m, int n, vector<vector<int>> picture) {
-	int number_of_area = 0;
-	int max_size_of_one_area = 0;
+	int number_of_area = 0; // 구역
+	int max_size_of_one_area = 0; // 가장 큰 구역 칸 개수
 
 	visit[100][100] = { false, };
 	num = 0, cnt = 0;
 	N = n;
 	M = m;
 
-	while (!visit[m][n]) {
-		//우측과 하단을 계속 스캔??
-			//DFS
-		visit[m][n] = true;
+	while (!visit[m - 1][n - 1]) {
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (!visit[i][j]) { // 오류 && !picture[i][j]) {
+					cnt = 0;
+					DFS(i, j, picture, picture[i][j]);
+					number_of_area++;
+					if (cnt > max_size_of_one_area) max_size_of_one_area = cnt;
+					cnt = 0;
+				}
+			}
+		}
 	}
 
 
