a37942ff16c36fb3ef1451f49e36f05993de95eb
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-06 20:29:40 +0900

    구현중

---
+++ 프로그래머스/단체사진 찍기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+int solution(int n, vector<string> data) {
+	//sstream
+
+
+	int answer = 0; // 모든 조건을 만족하는 경우의 수
+	return answer;
+}
