33a34e1f796f19cc7ecaaf0fe5fec0faa9c60963
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-07 23:16:44 +0900

    구현중

---
+++ 프로그래머스/멀쩡한 사각형.cpp
@@ -0,0 +1,12 @@
+﻿using namespace std;
+
+long long solution(int w, int h) {
+	long long answer = 1;
+	int all_sq = w * h;
+	int m_sq = 0;
+	//기울기를 w로 나눈다
+	//h의 크기만큼 네모를 뺀다.
+
+	answer = all_sq - m_sq;
+	return answer;
+}
