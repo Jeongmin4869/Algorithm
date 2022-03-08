3893f7ba2dd52d60947b2891526390b69d45e375
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-08 22:31:03 +0900

    구현중
    기울기를 사용해 구현중인데
    기울기로는 문제를 해결할 수 없는 것 같다.
    최소공배수로 풀어보기

--- 프로그래머스/멀쩡한 사각형.cpp
+++ 프로그래머스/멀쩡한 사각형.cpp
@@ -1,12 +1,18 @@
-﻿using namespace std;
+﻿#include <cmath>
+#include <algorithm>
 
+using namespace std;
+//최소공배수
 long long solution(int w, int h) {
 	long long answer = 1;
-	int all_sq = w * h;
-	int m_sq = 0;
-	//기울기를 w로 나눈다
-	//h의 크기만큼 네모를 뺀다.
+	long long all_sq = w * h;
+	long long m_sq = 0;
+	long long hi = max(w, h); long long we = min(w, h);
 
+	float a = sqrt(pow(we, 2) + pow(hi, 2));
+	a /= we;
+	float b = sqrt(pow(a, 2) - 1);
+	m_sq = ceil(b) * we;// 반올림
 	answer = all_sq - m_sq;
 	return answer;
 }
