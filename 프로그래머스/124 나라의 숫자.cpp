19e5c94127722310ca257585e93edaaab10ed0ed
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-10 20:32:34 +0900

    C++

---
+++ 프로그래머스/124 나라의 숫자.cpp
@@ -0,0 +1,15 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+
+string solution(int n) {
+	//3진법
+	string answer = "";
+	vector<string> nara = { "1","2","4" };
+	while (n > 0) {
+		n = n - 1;
+		answer.insert(0, nara[n % 3]);
+		n = n / 3;
+	}
+	return answer;
+}
