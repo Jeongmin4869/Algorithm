769d9847fbf791acc069e4ee0beecc9228ffbcfb
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-11 19:50:39 +0900

    C++

---
+++ 프로그래머스/오픈채팅방2.cpp
@@ -0,0 +1,31 @@
+﻿#include <string>
+#include <vector>
+#include <sstream>
+#include <unordered_map>
+using namespace std;
+
+vector<string> solution(vector<string> record) {
+	unordered_map<string, string> map;
+	vector<string> answer;
+	string id, state, name;
+	for (int i = 0; i < record.size(); i++) {
+		stringstream stream1;
+		stream1.str(record[i]);
+		stream1 >> state >> id >> name;
+		if (state != "Leave") map[id] = name;
+	}
+	for (auto a : record) {
+		string s = "";
+		stringstream stream1;
+		stream1.str(a);
+		stream1 >> state >> id;
+		if (state == "Change")
+			continue;
+		s += map[id] + "님이 ";
+		state == "Enter" ? s += "들어왔습니다." : s += "나갔습니다.";
+		answer.push_back(s);
+	}
+
+
+	return answer;
+}
