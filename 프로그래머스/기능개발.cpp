e572f1c0e55eafc777cbfb6259e0234f43147d53
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-12 09:59:01 +0900

    C++

---
+++ 프로그래머스/기능개발.cpp
@@ -0,0 +1,28 @@
+﻿#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+	vector<int> answer;
+	vector<int> days;
+
+	for (int i = 0; i < progresses.size(); i++) {
+		days.push_back(ceil((float)(100 - progresses[i]) / speeds[i]));
+	}
+	int count = 1, before = days[0];
+
+	for (int i = 1; i < days.size(); i++) {
+		if (before >= days[i]) {
+			count++;
+		}
+		else {
+			answer.push_back(count);
+			count = 1;
+			before = days[i];
+		}
+	}
+	answer.push_back(count); // 얘의 이유
+
+	return answer;
+}
