0c67e0d65362b4fc84967ba1bc3a42c413aad586
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-14 21:29:38 +0900

    구현중
    효율성 테스트는 성공했으나
    일부 문제들 통과 X
    
    주석 질문들 전부 블로그에 답해놓을것

---
+++ 프로그래머스/더 맵게.cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include <vector>
+#include <queue> //heap 구현
+//#include <cstdio>        // less
+#include <functional>    // greater
+
+using namespace std;
+//우선순위 큐를 사용
+//우선순위란?
+//최소 힙이란?
+//왜 배열대신 우선순위 큐를 사용했는가?
+
+int solution(vector<int> scoville, int K) {
+	int answer = 0;
+	priority_queue<int, vector<int>, greater<int>> pq;
+	sort(scoville.begin(), scoville.end());
+	for (int i = 0; i < scoville.size(); i++) {
+		pq.push(scoville[i]);
+	}
+
+	while (pq.top() < K) {
+		int n = pq.top(); pq.pop();
+		pq.push(n + pq.top() * 2); pq.pop();
+		answer++;
+	}
+
+
+
+	return answer;
+}
