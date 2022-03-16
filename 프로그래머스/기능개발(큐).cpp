c274d32ae90fdad118012d90c125a5d27452d65c
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-16 18:12:04 +0900

    C++

---
+++ 프로그래머스/기능개발(큐).cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+#include <cmath>
+#include <queue> 
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+	vector<int> answer;
+	queue<int> q;
+	for (int i = 0; i < progresses.size(); i++) {
+		q.push(ceil((float)(100 - progresses[i]) / speeds[i]));
+	}
+	int count = 1, before = q.front(); q.pop();
+
+	while (!q.empty()) {
+		if (before >= q.front()) {
+			count++;
+		}
+		else {
+			answer.push_back(count);
+			count = 1;
+			before = q.front();
+		}
+		q.pop();
+	}
+	answer.push_back(count);
+
+	return answer;
+}
