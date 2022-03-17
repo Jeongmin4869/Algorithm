3fad0ef1773149db836a8e034b63bfeaf6701ea7
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-17 19:03:38 +0900

    구현중

--- 프로그래머스/단체사진 찍기.cpp
+++ 프로그래머스/단체사진 찍기.cpp
@@ -1,37 +1,62 @@
 ﻿#include <string>
 #include <vector>
-#include <sstream>
-#include <unordered_map>
-
+#include <algorithm>
+#include <memory.h>
+#include <cstdlib> // abs
+#include <iostream>
 using namespace std;
 
-int count;
-vector<int> friends[8];
-
-//경우의 수 다 구하기??
-int dfs(int n, vector<string> data) {
-	for (int i = 0; i < data.size(); i++) {
-		char A = data[0], B = data[2];
-
-		switch (data[4]) {
-		case '=':
-			break;
-		case '<':
-			break;
-		case '>':
-			break;
-		}
-
-	}
-}
+int answer;
+vector<char> friends;
+vector<bool> visited;
+vector<string> datas;
+void dfs(int n, string inputstr);
+bool check(string str);
 
 // 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 int solution(int n, vector<string> data) {
-	count = 0;
-	friends = { A, C, F, J, M, N, R, T };
-	//sstream
+	answer = 0;
+	friends = { 'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T' };
+	visited.assign(8, false); // vector 초기화
+	datas = data;
+	dfs(0, "");
 
-
-	int answer = 0; // 모든 조건을 만족하는 경우의 수
 	return answer;
+}
+
+
+bool check(string str) {
+	for (auto data : datas) {
+		int idx1 = find(data.begin(), data.end(), data[0]) - data.begin();
+		int idx2 = find(data.begin(), data.end(), data[2]) - data.begin();
+		switch (data[3]) {
+		case '=':
+			if (abs(idx1 - idx2) != data[4] + 1) return false;
+			break;
+		case '<': // 미만
+			if (abs(idx1 - idx2) >= data[4] + 1) return false;
+			break;
+		case '>': // 초과
+			if (abs(idx1 - idx2) <= data[4] + 1) return false;
+			break;
+		}
+	}
+	return true;
+}
+
+void dfs(int n, string inputstr) {
+	if (n == 8) {
+		if (check(inputstr)) answer++;
+		return;
+	}
+
+	//경우의 수 다 구하기    
+	for (int i = 0; i < visited.size(); i++) {
+		string str = inputstr + friends[i];
+		visited[i] = true;
+		dfs(n + 1, str);
+		visited[i] = false; // 숫자의 순서를 바꿔주기 위함 .. .
+	}
+
+	return;
 }
