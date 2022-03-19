1aa4be2e1643e56d6e08432cd4633a67bd3fec0d
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-19 16:55:11 +0900

    C++
    다른사람 풀이 참고하여 해결

---
+++ 프로그래머스/타겟 넘버.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+vector<int> datas;
+int answer;
+int tarN;
+void dfs(int n, int before);
+
+int solution(vector<int> numbers, int target) {
+	answer = 0;
+	datas = numbers;
+	tarN = target;
+	dfs(0, 0);
+
+	return answer;
+}
+
+//dfs란 뭘까...
+void dfs(int n, int before) {
+	if (n == datas.size()) {
+		if (before == tarN) answer++;
+		return;
+	}
+
+	dfs(n + 1, before + datas[n]);
+	dfs(n + 1, before - datas[n]);
+	return;
+}
