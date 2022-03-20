3bdb689b6e5ce0f7ec868c4b26da81dd4724c0b0
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-20 20:52:17 +0900

    구현중

---
+++ 프로그래머스/짝지어 제거하기.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+//#include <sstream>
+
+using namespace std;
+
+int solution(string s)
+{
+	int answer = 0;
+	//stringstream str1;
+	for (int i = 0; i < s.length() - 1; i++) {
+		if (s[i] == s[i + 1]) {
+			answer++;
+			i = 0;
+			//s변경
+		}
+	}
+	return answer;
+}
