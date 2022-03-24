f92df41b1d47ab53c5274886b22625e72241c137
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-24 20:24:34 +0900

    구현중

--- 프로그래머스/행렬 테두리 회전하기.cpp
+++ 프로그래머스/행렬 테두리 회전하기.cpp
@@ -1,5 +1,6 @@
 ﻿#include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -17,44 +18,51 @@ vector<vector<int>> set_v(int a, int b) {
 }
 
 //x1, y1, x2, y2
+//세로가 X, 가로가 Y
 vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
 	vector<int> answer;
 	vector<vector<int>> v = set_v(rows, columns);
 	int min = 0;
-	int temp, len, x, y;
+	int temp, before, len, x, y, x1, x2, y1, y2;
 	for (auto q : queries) {
-		min = 0;
-		len = (x2 - x1) * (y2 - y1) - (x2 - x1 - 2) * (y2 - y1 - 2);
-		x = q[0]; y = q[1];
-		temp = v[y][x++];
+		min = 100;
+		x1 = q[0]; y1 = q[1];
+		x2 = q[2]; y2 = q[3];
+		len = (x2 - x1 + 1) * (y2 - y1 + 1) - (x2 - x1 - 1) * (y2 - y1 - 1);
+		x = q[0] - 1; y = q[1] - 1;
+		before = v[x][y++];
+		min = before;
 		for (int i = 0; i < len; i++) {
-			v[y][x] = temp;
-			if (y == q[i]) { // ->
-				if (x + 1 > q[2]) y++;
+			temp = v[x][y];
+			v[x][y] = before;
+			before = temp;
+			if (x == q[0] - 1) { // ->
+				if (y + 1 > q[3] - 1) x++;
+				else y++;
+			}
+
+			else if (y == q[3] - 1) { // 밑
+				if (x + 1 > q[2] - 1) y--;
 				else x++;
 			}
 
-			else if (x == q[2]) { // 밑
-
+			else if (x == q[2] - 1) { //<-
+				if (y - 1 < q[1] - 1) x--;
+				else y--;
 			}
 
-			else if (y == q[3]) {
-				< -
-
-
+			else if (y == q[1] - 1) {// 위
+				if (x - 1 > q[0] - 1) y++;
+				else x--;
 			}
-
-			else if (x == q[0]) {
-				위
-
-
-			}
-			temp = v[y][x];
+			if (temp < min) min = temp;
 
 		}
+		if (temp < min) min = temp;
 		answer.push_back(min);
+		min = 100;
 	}
 
-
+	//이게 왜 돼..?? 진짜 킹받아.. -> 응안돼..
 	return answer;
 }
