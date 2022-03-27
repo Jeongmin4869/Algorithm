994723a8f723c915f9e099813e67888f27c75f4a
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-27 20:20:09 +0900

    구현중
    모든 경우의 수를 구해야 하는지 고민중..!!

--- 프로그래머스/메뉴 리뉴얼.cpp
+++ 프로그래머스/메뉴 리뉴얼.cpp
@@ -1,12 +1,36 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
-//1. dfs ?
-//N^2?
+vector<int> c;
+vector<char> al;
+//dfs.. ????!!!
 vector<string> solution(vector<string> orders, vector<int> course) {
 	vector<string> answer;
-	int count = 0;
+	c = course;
+
+	for (int i = 0; i < orders.size(); i++) {
+		for (int j = 0; j < orders[i].size(); j++) {
+			auto p = find(al.begin(), al.end(), orders[i][j]);
+			if (p != al.end()) al.push_back(orders[i][j]);
+		}
+	}
+
+	sort(orders.begin(), orders.end());
 
 	return answer;
+}
+
+
+void dfs(string s, vector<string> answer) {
+	auto p = find(c.begin(), c.end(), s.size());
+	if (p != c.end()) {
+		answer.push_back(s);
+		return;
+	}
+
+
+	return;
+
 }
