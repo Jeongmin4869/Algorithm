6041cb005686e84974265e5bccf7a0eefa6c1ddc
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-28 19:19:21 +0900

    구현중
    dfs로 순열 구하는중..
    이제 비교문을 짜야겠다

--- 프로그래머스/메뉴 리뉴얼.cpp
+++ 프로그래머스/메뉴 리뉴얼.cpp
@@ -1,36 +1,51 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <set>
+#include <iostream>
 
 using namespace std;
 vector<int> c;
 vector<char> al;
+vector<bool> visited;
+//next_permutation?
+void dfs(string str, vector<string> &answer) {
+	int n = str.size();
+	auto p = find(c.begin(), c.end(), n);
+	if (p != c.end()) {
+		cout << ",";
+		answer.push_back(str);
+		return;
+	}
+
+	//dfs 순열 구하기(중복허용X)
+	for (int i = 0; i < al.size(); i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			string str2 = str + al[i];
+			dfs(str2, answer);
+			visited[i] = false;
+		}
+	}
+	return;
+
+}
 //dfs.. ????!!!
 vector<string> solution(vector<string> orders, vector<int> course) {
 	vector<string> answer;
+	set<char> s;
 	c = course;
 
 	for (int i = 0; i < orders.size(); i++) {
 		for (int j = 0; j < orders[i].size(); j++) {
-			auto p = find(al.begin(), al.end(), orders[i][j]);
-			if (p != al.end()) al.push_back(orders[i][j]);
+			s.insert(orders[i][j]);
 		}
 	}
+	al.assign(s.begin(), s.end());
 
-	sort(orders.begin(), orders.end());
+	visited.assign(al.size(), false);
+
+	dfs("", answer);
 
 	return answer;
-}
-
-
-void dfs(string s, vector<string> answer) {
-	auto p = find(c.begin(), c.end(), s.size());
-	if (p != c.end()) {
-		answer.push_back(s);
-		return;
-	}
-
-
-	return;
-
 }
