bed95d3704561c76e1feb86e9987c38ec3a0b81a
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-03-31 11:33:46 +0900

    구현중
    조합을 어떻게 구해야 하는가...

--- 프로그래머스/메뉴 리뉴얼.cpp
+++ 프로그래머스/메뉴 리뉴얼.cpp
@@ -8,33 +8,39 @@ using namespace std;
 vector<int> c;
 vector<char> al;
 vector<bool> visited;
+//N:3, M:2 // N은 원소의 개수, M은 뽑을 개수
+
 //next_permutation?
 // 3개로 구성된 유효한 코스가 {ABC}가 있다면 
 // 4개로 구성된 코스는 {ABC + 'C 다음 알파벳'}의 경우로만 탐색해주면 됩니다
+
 void dfs(string str, vector<string> &answer, vector<string> &orders) {
 	int n = str.size();
-	auto p = find(c.begin(), c.end(), n);
+	auto p = find(c.begin(), c.end(), str.size());
 	if (p != c.end()) {
+
 		bool b = true;
-		/*
-		for(int i=0; i<orders.size(); i++){
-			for(int j=0; j<str.size(); j++){
-				auto p2 =orders[i].find(str[j]);
-				if(p2 == string::npos){
+		int count = 0;
+		for (int i = 0; i < orders.size(); i++) {
+			count++;
+			b = true;
+			for (int j = 0; j < str.size(); j++) {
+				auto p2 = orders[i].find(str[j]);
+				if (p2 == string::npos) {
 					b = false;
 					break;
 				}
 			}
-			if(b) answer.push_back(str);
 		}
-		//auto p = find(c.begin(), c.begin() + answer[i].size(),n);
-		*/
-		if (b) answer.push_back(str);
+		if (b && count > 1) answer.push_back(str);
+
 		return;
 	}
 
-	//dfs 순열 구하기(중복허용X)
-	for (int i = 0; i < al.size(); i++) {
+	//dfs 순열 구하기(순열은 중복만을 X할뿐 )
+	// 순열로하면안된다 -> 조합
+	// int i=str.size() 해주었음.. // 이럼안됨!
+	for (int i = str.size(); i < al.size(); i++) {
 		if (!visited[i]) {
 			visited[i] = true;
 			string str2 = str + al[i];
