199cb22d48bdec66cfc65e9657943fa7288c9dc2
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-04-02 01:07:00 +0900

    구현중

--- 프로그래머스/괄호 변환.cpp
+++ 프로그래머스/괄호 변환.cpp
@@ -1,11 +1,21 @@
 ﻿#include <string>
 #include <vector>
-
+#include <stack>
 using namespace std;
 
 //괄호는 stack으로 해곃하고
 
 string solution(string p) {
 	string answer = "";
-	return answer;
+	stack<char> s;
+
+	for (int i = 0; i < p.size(); i++) {
+		if (p[i] == '(') s.push('(');
+		else {
+			if (s.top() == '(') s.pop();
+			else break;
+		}
+	}
+
+	return answer = p;
 }
