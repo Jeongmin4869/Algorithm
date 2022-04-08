dbdee00406742e3707dfdf5c40ed0604b6b9921d
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-04-08 22:38:40 +0900

    구현중

--- 프로그래머스/메뉴 리뉴얼.cpp
+++ 프로그래머스/메뉴 리뉴얼.cpp
@@ -9,7 +9,8 @@ vector<int> c;
 vector<char> al;
 vector<bool> visited;
 //N:3, M:2 // N은 원소의 개수, M은 뽑을 개수
-
+//같은 글자수 중 가장 많이 주문한 답만 출력, 수가 같다면 같이.. 
+//AD가 세 번, CD가 세 번, ACD가 두 번, ADE가 두 번, XYZ 가 두 번
 //next_permutation?
 // 3개로 구성된 유효한 코스가 {ABC}가 있다면 
 // 4개로 구성된 코스는 {ABC + 'C 다음 알파벳'}의 경우로만 탐색해주면 됩니다
@@ -21,6 +22,7 @@ void dfs(string str, vector<string> &answer, vector<string> &orders) {
 
 		int count = 0;
 		for (int i = 0; i < orders.size(); i++) {
+			count++;
 			for (int j = 0; j < str.size(); j++) {
 				auto p2 = orders[i].find(str[j]);
 				if (p2 == string::npos) {
@@ -29,8 +31,10 @@ void dfs(string str, vector<string> &answer, vector<string> &orders) {
 				}
 			}
 		}
-		if (count > 1) answer.push_back(str);
-
+		if (count > 1) {
+			//코스메뉴 갯수당 count값이 가장 큰것만
+			answer.push_back(str);
+		}
 		//if(b) answer.push_back(str);
 		return;
 	}
