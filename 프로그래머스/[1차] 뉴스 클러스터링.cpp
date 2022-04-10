4455ea7193aa5dcc449c4f72cfb817d5da0e8842
Mingco <45901815+Jeongmin4869@users.noreply.github.com>
2022-04-10 22:41:46 +0900

    구현중

--- 프로그래머스/[1차] 뉴스 클러스터링.cpp
+++ 프로그래머스/[1차] 뉴스 클러스터링.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
+#include <iostream>
 using namespace std;
 
 vector<string> v(string str) {
@@ -8,23 +9,34 @@ vector<string> v(string str) {
 	vector<string> vec;
 	for (int i = 1; i < str.size(); i++) {
 		if (tolower(str[i]) < 'a' || tolower(str[i]) > 'z') continue;
-		string s = to_string(before) + to_string(tolower(str[i]));
+		string s;
+		s.append(1, before);
+		s.append(1, tolower(str[i]));
 		vec.push_back(s);
-		before = str[i];
+		before = tolower(str[i]);
 	}
 	return vec;
 
 }
 
 int solution(string str1, string str2) {
-	int answer = 0;
+	float answer = 0;
+	int inter, uni; // 교, 합집합
+
 	vector<string> vec1, vec2;
 	vec1 = v(str1);
 	vec2 = v(str2);
+	uni += vec1.size() + vec2.size();
+
 	for (int i = 0; i < vec1.size(); i++) {
-		//if(find(vec2.begin(), vec2.end(), vec1[i]) !)
+		if (find(vec2.begin(), vec2.end(), vec1[i]) != vec2.end()) {
+			inter++;
+			vec1.erase(vec1.begin() + i--);
+		}
 	}
+	uni -= inter;
+	answer = (float)inter / uni;
+	//cout << answer;
 
-
-	return answer;
+	return (int)(answer * 65536) / 1;
 }
