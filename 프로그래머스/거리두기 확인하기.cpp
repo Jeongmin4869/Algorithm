65ffb1b9fadf41ec0b4aed604f09b715d1c61405
ljm4135@naver.com <45901815+Jeongmin4869@users.noreply.github.com>
2022-04-14 20:29:05 +0900

    구현중

--- 프로그래머스/거리두기 확인하기.cpp
+++ 프로그래머스/거리두기 확인하기.cpp
@@ -1,30 +1,44 @@
 #include <string>
 #include <vector>
-
+#include <string.h> // memset
 using namespace std;
+bool visited[100][100];
 int dx[4];
 int dy[4];
-int N, M; // x축, y축
-void dfs(int x, int y, vector<string> place) {
+int N, M; // x축, y축  [y][x]
+int dfs(int x, int y, vector<string> place, int count) {
+	if (count == 2) return 0;
 
 	for (int i = 0; i < 4; i++) {
 		x += dx[i];
 		y += dy[i];
 		if ((x > 0 && y > 0) && (x < N && y < M)) {
-			//
+			if (place[y][x] != 'X' && !visited[y][x]) {
+				visited[y][x] = true;
+				dfs(x, y, place, count++);
+				visited[y][x] = false;
+			}
 		}
 	}
-
+	return 1;
 }
 
 vector<int> solution(vector<vector<string>> places) {
 	vector<int> answer;
 	dx[0] = 1; dx[1] = 0; dx[2] = -1; dx[3] = 0;
 	dy[0] = 0; dy[1] = 1; dy[2] = 0; dy[3] = -1;
-	for (int i = 0; i < places.size(); i++) {
-		N = places[i][0].size();
-		M = places[i].size();
-		//dfs
+	for (auto place : places) {
+		memset(visited, false, sizeof(visited));
+		N = place[0].size();
+		M = place.size();
+		for (int y = 0; y < M; y++) {
+			for (int x = 0; x < N; x++) {
+				if (place[y][x] == 'P') {
+					int n = dfs(x, y, place, 0);
+					if (n == 0) break
+				}
+			}
+		}
 	}
 
 
