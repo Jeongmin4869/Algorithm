9129eb60c707b1b807d3cb3808122f445b81d2a4
jeongmin4869 <ljm4135@naver.com>
2022-05-12 06:24:42 +0900

    C++(정답봄)
    구현이 힘들어 정답을 보았다!
    블로그에 정리하면서 다시 공부할 예정

--- 프로그래머스/메뉴 리뉴얼.cpp
+++ 프로그래머스/메뉴 리뉴얼.cpp
@@ -6,50 +6,47 @@
 #include <map>
 
 using namespace std;
-vector<int> c;
-vector<bool> visited;
 map<string, int> hashMap;
 //정답 봄
 
-void dfs(int depth, string str, string order) {
+void dfs(int targetnum, string str, string order) {
 	if (str.size() == targetnum) {
 		hashMap[str] += 1;
+		return;
 	}
 
+	//dfs 순열 구하기(순열은 중복만을 X할뿐 )
 	// 순열로하면안된다 -> 조합
-	for (int i = 0; i < order.size(); i++) {
-		dfs(depth, order.substr(i + 1), str + order[i]);
-	}
+	// int i=str.size() 해주었음.. // 이럼안됨!
+	for (int i = 0; i < order.size(); i++)
+		dfs(targetnum, str + order[i], order.substr(i + 1));
+	return;
 }
-//dfs.. ????!!!
+
 vector<string> solution(vector<string> orders, vector<int> course) {
 	vector<string> answer;
-	set<char> s;
-	c = course;
 
+	//없으면 오류
 	for (string &order : orders)
 		sort(order.begin(), order.end());
 
-	visited.assign(al.size(), false);
-	for (int i = 0; i < course.size(); i++) {
-		dfs(course[i], "", 0);
+
+	for (int cur : course) {
+		for (string order : orders)
+			dfs(cur, "", order);
 
 		int sup = 0;
-		for (auto m : hashMap) {
+		for (auto m : hashMap)
 			sup = max(sup, m.second);
-		}
 
-		for (auto m : hashMap) {
-			if (sup > 2 && m.second == sup)
+		for (auto m : hashMap)
+			if (sup >= 2 && m.second == sup)
 				answer.push_back(m.first);
-		}
 
-		visited.assign(al.size(), false);
 		hashMap.clear();
 	}
 
 
-
-	//sort(answer.begin(), answer.end());
+	sort(answer.begin(), answer.end());
 	return answer;
 }
