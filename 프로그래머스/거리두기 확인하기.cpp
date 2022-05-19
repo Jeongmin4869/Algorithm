98cc9965a62c7092e51d21397ab7a2730e3595e4
jeongmin4869 <ljm4135@naver.com>
2022-05-20 06:33:13 +0900

    구현중

--- 프로그래머스/거리두기 확인하기.cpp
+++ 프로그래머스/거리두기 확인하기.cpp
@@ -1,6 +1,7 @@
 ﻿#include <string>
 #include <vector>
 #include <string.h> // memset
+#include <iostream>
 using namespace std;
 bool visited[100][100];
 
@@ -9,32 +10,29 @@ bool visited[100][100];
 
 int dx[4];
 int dy[4];
-int N, M; // x축, y축  [y][x]
 bool isSafe;
 void dfs(int y, int x, vector<string> place, int count) {
+	visited[y][x] = true;
 	if (count > 2 || place[y][x] == 'X') {
-		isSafe = false;
 		return;
 	}
 	else {
 		if (count != 0 && place[y][x] == 'P') {
-			isSafe = true;
+			isSafe = false;
 			return;
 		}
 
 		for (int i = 0; i < 4; i++) {
 			x += dx[i];
 			y += dy[i];
-			if ((x >= 0 && y >= 0) && (x < N && y < M)) {
+			if ((x >= 0 && y >= 0) && (x < 5 && y < 5)) {
 				if (!visited[y][x]) {
-					visited[y][x] = true;
-					dfs(x, y, place, count++);
-					visited[y][x] = false;
+					dfs(x, y, place, count + 1);
 				}
 			}
 		}
 	}
-	return;
+	//return;
 }
 
 //P좌표로 풀기? 탐색으로 풀기?
@@ -42,23 +40,22 @@ vector<int> solution(vector<vector<string>> places) {
 	vector<int> answer;
 	dx[0] = 1; dx[1] = 0; dx[2] = -1; dx[3] = 0;
 	dy[0] = 0; dy[1] = 1; dy[2] = 0; dy[3] = -1;
-	for (auto place : places) {
+	for (vector<string> place : places) {
 		isSafe = true;
 		memset(visited, false, sizeof(visited));
-		N = place[0].size();
-		M = place.size();
-		for (int y = 0; y < place.size(); y++) {
-			for (int x = 0; x < place[y].size(); x++) {
+		for (int y = 0; y < 5; y++) {
+			for (int x = 0; x < 5; x++) {
+				cout << place[y][x];
 				if (place[y][x] == 'P') {
+					//isSafe = true;
 					dfs(y, x, place, 0);
-					if (isSafe == 0) break;
+					if (!isSafe) break;
 				}
 			}
-			if (isSafe == 0) break;
+			if (!isSafe) break;
 		}
 		isSafe == true ? answer.push_back(1) : answer.push_back(0);
+		cout << endl;
 	}
-
-
 	return answer;
 }
