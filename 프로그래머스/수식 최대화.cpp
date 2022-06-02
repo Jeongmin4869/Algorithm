6c1890c634275e34b40271e603420297ac58674f
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2022-06-02 14:34:23 +0900

    구현중

--- 프로그래머스/수식 최대화.cpp
+++ 프로그래머스/수식 최대화.cpp
@@ -9,6 +9,9 @@ using namespace std;
 //음수라면 해당 숫자의 절댓값으로 변환하여 제출
 //연산 뒤에 피연산자가 음수인 연산을 처리하는 +와 * 연산을 한개식 더 정의
 //6가지 경우의 수
+
+//스택에 숫자와 연산자를 넣다가, 현재 계산해야 하는 연산자 일 시 
+// 숫자 스택에 위의 2개, 연산자 스택의 위의 1개가 현재 계산해야 하는 식 이다
 int dfs(string expression){
     int n;
     return n;
@@ -36,17 +39,25 @@ long long solution(string expression) {
                                     {'+', '-', '*'}, {'+', '*', '-'},
                                     {'*', '-', '+'}, {'*', '+', '-'}};
     
-    int max_v = 0;
+    int max_val = 0;
+    int val;
     int num;
+    char sign;
     stack<int> dial;
     stack<char> ca;
-    //stack<string> ? 
-    char sign;
-    stringstream ss(expression);
     
     for(auto prior : priors){
+        //while 문자열 자르기
+        stringstream ss(expression);
+        
         for(int i=0; i<prior.size(); i++){
+            ss>dial;
+            ss>ca;
+            
+        
         }
+        
+        if(val > max_val) max_val = val;        
     }
     
     long long answer = 0;
