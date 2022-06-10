7f2486233b3ef6cd91cb2966899263445624badf
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2022-06-10 17:27:15 +0900

    구현중

--- 프로그래머스/수식 최대화.cpp
+++ 프로그래머스/수식 최대화.cpp
@@ -14,71 +14,73 @@ using namespace std;
 //스택에 숫자와 연산자를 넣다가, 현재 계산해야 하는 연산자 일 시 
 // 숫자 스택에 위의 2개, 연산자 스택의 위의 1개가 현재 계산해야 하는 식 이다
 
-int calc(int a, int b, char c) {
-	int n = 0;
-	switch (c) {
-	case '-':
-		n = a - b;
-		break;
-	case '+':
-		n = a + b;
-		break;
-	case '*':
-		n = a * b;
-		break;
-	}
-	return n;
+int calc(int a, int b, char c){
+    int n = 0;
+    switch(c){
+        case '-':
+            n = a-b;
+            break;
+        case '+':
+            n = a+b;
+            break;
+        case '*':
+            n = a*b;
+            break;
+    }
+    return n;
 }
 
 long long solution(string expression) {
-	//순서 벡터? //숫자따로 char 따로??
-	vector<vector <char>> priors = { {'-', '+', '*'},{'-', '*', '+'},
-									{'+', '-', '*'}, {'+', '*', '-'},
-									{'*', '-', '+'}, {'*', '+', '-'} };
-
-	int max_val = 0;
-	int val = 0;
-	int num;
-	char sign;
-	stack<int> dial;
-	stack<char> ca;
-	string ex = expression;
-
-	for (auto prior : priors) {
-		//while 문자열 자르기
-		ex = expression;
-		//cout << "ex : " << ex << endl;
-		for (int i = 0; i < prior.size(); i++) {
-			stringstream ss(ex);
-			while (ss >> num) {  // while문 시작부터 이상해.
-				dial.push(num);
-				if (ss >> sign) {
-					ca.push(sign);
-					if (ca.top() == prior[i]) {
-						int num2, num = dial.top();
-						ss >> num2;
-						dial.pop();
-						cout << calc(num, num2, sign) << endl;
-						dial.push(calc(num, num2, sign));
-					}
-				}
-			}
-
-			//문자열 생성
-			ex = "";
-			while (!dial.empty()) {
-				ex.insert(0, to_string(dial.top()));
-				dial.pop();
-				if (!ca.empty()) {
-					ex.insert(0, 1, ca.top());
-					ca.pop();
-				}
-			}
-
-		}
-		if (val > max_val) max_val = val;
-	}
-
-	long long answer = 0;
-	return answer;
+    //순서 벡터? //숫자따로 char 따로??
+    vector<vector <char>> priors = {{'-', '+', '*'},{'-', '*', '+'},
+                                    {'+', '-', '*'}, {'+', '*', '-'},
+                                    {'*', '-', '+'}, {'*', '+', '-'}};
+    
+    int max_val = 0;
+    int val = 0;
+    int num;
+    char sign;
+    stack<int> dial;
+    stack<char> ca;
+    string ex = expression;
+    
+    for(auto prior : priors){
+        //while 문자열 자르기
+        ex = expression;
+        for(int i=0; i<prior.size(); i++){
+            //cout << "ex : " << ex << endl;
+            stringstream ss(ex);
+            while(ss){  // 이게 왜 돼?
+                if(ss >> num) {
+                    dial.push(num);
+                }
+                if(ss >> sign){
+                    ca.push(sign); 
+                    if(ca.top() == prior[i]){
+                        int num2, num = dial.top();
+                        ss >> num2;
+                        dial.pop();
+                        cout << calc(num, num2, sign) << endl;
+                        dial.push(calc(num, num2, sign));
+                    }  
+                }
+            }
+            
+            //문자열 생성
+            ex = "";
+            while(!dial.empty()){
+                ex.insert(0,to_string(dial.top()));
+                dial.pop();
+                if(!ca.empty()){
+                    ex.insert(0,1,ca.top());
+                    ca.pop();
+                }
+            }
+            
+        }
+        if(val > max_val) max_val = val;        
+    }
+    
+    long long answer = 0;
+    return answer;
 }
