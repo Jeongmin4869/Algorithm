eec50384c4b91543eed4db4bf4b1f985c82d6e5c
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2022-08-10 20:34:26 +0900

    구현중 집가서수정

--- 프로그래머스/조이스틱.cpp
+++ 프로그래머스/조이스틱.cpp
@@ -4,60 +4,67 @@
 #include <iostream>
 using namespace std;
 
-//A로 전부 시작
-
 int sol1(char c, vector<char> al){ // c가 목표알파벳
     int val = 0;
     int p2 = find(al.begin(), al.end(),c) - al.begin();
-    p2 = al.size() - p2;
+    if(p2 > al.size()/2){
+        p2 = al.size() - p2;
+    }
     return p2;
 }
 
+
 int solution(string name) {
     int answer = 0;
-    int count = -1;
-    int a_count = 0;
-    int len = 0;
-    int max_a=0, max_l = 0;
-    //vector<char> v;
+    int count = 0;
+    int a_len = 0, max_a_len = 0;
+    int a_idx = 0;
     vector<char> al = {'A','B','C','D','E','F','G','H','I','J','K','L','M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     
-    //정방향 ... 
-    //가장 큰 A배열 eln 구해서 나중에 len -1 을 정방향으로 구한거에서 빼주기
+    // 1. 가장 긴 A len 구하기
     for(int i=0; i<name.size(); i++){
-        
-        if(name[i] == 'A'){
-            a_count++;
-            if(len == -1) len = i;
+        while(name[i] == 'A'){
+            a_len ++ ;
+            i++;
         }
-        else {
-            if(len <= a_count){
-                if(max_a<a_count){
-                    max_a = a_count;
-                    max_l = len;
-                }
-            }
-            a_count = 0;
-            len = -1;
+        if(max_a_len < a_len ) {
+            max_a_len = a_len;
+            a_idx = i;
         }
+        a_len = 0;
+    }
+    
+    // 2. 가장 긴 A를 기준으로 왼, 오 길이 구하기
+    int l_len=0, r_len=0;
+    if(max_a_len == 0) l_len = name.size();
+    else {
+        l_len = a_idx - max_a_len;
+        r_len = name.size() - a_idx;
+    }
+    cout << l_len << " , " << r_len << endl;
+    //3가지 경우의 수 구하기. 1)순서대로 2)순서대로갔다가반대로 3)반대로갔다가순서대로
+    int move = 0;
+    if(max_a_len==0 || r_len == 0 && l_len > 0){
+        //처음이랑 끝에 이어진게 가장 긴 경우
+        count += l_len-1;
+    }
+    
+    move = count;
+    else if (l_len <= max_a_len && l_len <= r_len) {
+        if(l_len == 0)  
+            count += r_len;
+        else
+            count += (2*l_len + r_len - 2);
+    }
+    else if(r_len < l_len){ 
+         count += (2*r_len + l_len - 1);  
+    }
+    
 
-        count ++; // 커서이동
-        int num1, num2;
-        num1 = find(al.begin(), al.end(),name[i]) - al.begin();
-        num2 = sol1( name[i], al);
-        count = count + min(num1, num2);
-        //v.push_back(name[i]);
-    }
-    
-    if(max_a != 0 && max_l > 0){
-        cout << max_a << endl;
-        cout << max_l << endl;
-        count = count - max_a + max_l -1;
-    }
-    else if (max_a != 0 && max_l == 0){
-        count = count - max_a + 1;
-    }
-    
+    //4. 세로이동방향 구하기
+    for(auto c : name){
+        count += sol1(c, al);
+    } 
     
     return count;
 }
