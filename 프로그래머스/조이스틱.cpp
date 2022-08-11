a18942eaa9eddc27981a52ae7ac89d160559696b
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2022-08-11 10:32:58 +0900

    C++

--- 프로그래머스/조이스틱.cpp
+++ 프로그래머스/조이스틱.cpp
@@ -1,10 +1,9 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 using namespace std;
 
-int sol1(char c, string al){ // c가 목표알파벳
+int sol1(char c, string al){ 
     int val = 0;
     int p2 = find(al.begin(), al.end(),c) - al.begin();
     if(p2 > al.size()/2){
@@ -13,27 +12,28 @@ int sol1(char c, string al){ // c가 목표알파벳
     return p2;
 }
 
-
 int solution(string name) {
     int answer = 0;
-    int count = 0;
-    int a_len = 0, max_a_len = 0;
-    int a_idx = 0;
+    int idx, move = 0;
+    int l_len = 0, r_len = 0;
     string al = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     // 1. 가장 긴 A len 구하기
-    
+    move = name.size() -1; // 정방향
     for(int i=0; i<name.size(); i++){
-        int idx = i+1;
+        idx = i+1;
         while(name[idx] == 'A'){
             idx++;
         }
+        l_len = i; 
+        r_len = name.size() - idx;
+        move = min(move, l_len *2 + r_len ); // 정방향 -> 반대
+        move = min(move, r_len *2 + l_len ); // 반대 -> 정방향
     }
     
-
     //4. 세로이동방향 구하기
     for(auto c : name){
-        count += sol1(c, al);
+        move += sol1(c, al);
     } 
     
-    return count;
+    return move;
 }
