7a29e93a46c7a7df865672d4bf2ef1fc3668ab2c
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2022-08-11 10:38:05 +0900

    C++

--- 프로그래머스/조이스틱.cpp
+++ 프로그래머스/조이스틱.cpp
@@ -6,19 +6,18 @@ using namespace std;
 int sol1(char c, string al){ 
     int val = 0;
     int p2 = find(al.begin(), al.end(),c) - al.begin();
-    if(p2 > al.size()/2){
+    if(p2 > al.size()/2)
         p2 = al.size() - p2;
-    }
     return p2;
 }
 
 int solution(string name) {
     int answer = 0;
     int idx, move = 0;
-    int l_len = 0, r_len = 0;
     string al = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    // 1. 가장 긴 A len 구하기
+
     move = name.size() -1; // 정방향
+    //모든 A 구하기
     for(int i=0; i<name.size(); i++){
         idx = i+1;
         while(name[idx] == 'A'){
@@ -35,5 +34,5 @@ int solution(string name) {
         move += sol1(c, al);
     } 
     
-    return move;
+    return answer = move;
 }
