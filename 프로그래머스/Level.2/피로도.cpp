83cc79027ee62dab349adf0fa6710ebc25b5f4e8
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2022-10-01 22:12:35 +0900

    Create 피로도.cpp
    C++

---
+++ 프로그래머스/Level.2/피로도.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int s;
+int answer = 0;
+vector<bool> visited;
+
+void dfs(int len, int count, int HP, vector<vector<int>> dungeons){
+    if(len == s){
+        answer<count?answer=count:answer;
+        return;
+    }
+    
+    for(int i=0; i<s; i++){
+        if(!visited[i]){
+            visited[i] = true;
+            if(HP < dungeons[i][0])
+                dfs(len+1, count , HP-dungeons[i][1],dungeons);
+            else
+                dfs(len+1, count+1, HP-dungeons[i][1],dungeons);
+            visited[i] = false;
+        }
+    }
+}
+
+int solution(int k, vector<vector<int>> dungeons) {
+    
+    s = dungeons.size();
+    visited.assign(8,false);
+    dfs(0,0,k,dungeons);
+
+    
+    return answer;
+}
