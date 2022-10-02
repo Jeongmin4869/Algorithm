b9de59ec425685b68e6977894adccb826abc6fce
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2022-10-02 21:32:48 +0900

    C++

--- 프로그래머스/Level.2/n^2 배열 자르기.cpp
+++ 프로그래머스/Level.2/n^2 배열 자르기.cpp
@@ -1,3 +1,5 @@
+/****  효율성 0 ****/
+/*
 #include <string>
 #include <vector>
 using namespace std;
@@ -25,4 +27,26 @@ vector<int> solution(int n, long long left, long long right) {
         v.push_back(answer[i]);
     }
     return v;
+}
+*/
+
+
+/**** 효율성 통과 풀이 ****/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+
+    
+    int num = left/(long long)n;
+    for(long long i=left; i<=right; i++){
+        if(i%n <= num) answer.push_back(num+1);
+        else answer.push_back(i%n+1);
+        
+        if(i%n == n-1) num++;
+    }
+    return answer;
 }
