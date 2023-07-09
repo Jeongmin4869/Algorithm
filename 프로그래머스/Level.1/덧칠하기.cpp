8cb143e88b1d1ab035b4842c06907eabfc5e4e8e
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-07-09 17:23:30 +0900

    C++

--- 프로그래머스/Level.1/덧칠하기.cpp
+++ 프로그래머스/Level.1/덧칠하기.cpp
@@ -9,7 +9,7 @@ int solution(int n, int m, vector<int> section) {
     for(int i=0; i<section.size(); i++){
         if(paint < section[i]){
             answer += 1;
-            paint = section[i] - 1 + m;
+            paint = section[i] - 1 + m; // index가 0 부터 시작하기 때문에 -1
         }
     }
     return answer;
