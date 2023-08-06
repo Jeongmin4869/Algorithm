aced46234a05d3d654822028ad9f80a00ceb8088
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-08-06 18:20:17 +0900

    C++

---
+++ 프로그래머스/Level.2/N개의 최소공배수.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+GCD = 최대공약수
+LCM = 최소공배수
+*/
+
+int gcd(int n, int m){ // 유클리드호제법
+    if(n%m == 0) return m;
+    return gcd(m, n%m);
+}
+
+int solution(vector<int> arr) {
+    int answer = 0;
+    int num = arr[0]; // 이전에 구한 최대공배수
+    for(int i=1; i<arr.size(); i++){
+        int g = gcd(num, arr[i]); // 최대공약수
+        num = num*arr[i]/g; // 최대공배수
+    }
+    return answer = num;
+}
