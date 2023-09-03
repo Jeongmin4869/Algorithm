2d1f159640a367f754d3ad50f8292a74a5ff0d43
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-09-03 21:33:48 +0900

    C++
    해결완료

--- 프로그래머스/Level.2/할인 행사.cpp
+++ 프로그래머스/Level.2/할인 행사.cpp
@@ -1,3 +1,4 @@
+/*
 #include <string>
 #include <vector>
 #include <map>
@@ -27,6 +28,41 @@ int solution(vector<string> want, vector<int> number, vector<string> discount) {
             m[discount[i+10]] += 1;
         }
         else if(signin){
+            answer += 1;
+        }
+    }
+    // 원하는 제품을 모두 할인 받을 수 있는 회원등록 날짜의 총 일수
+    return answer;
+}
+
+*/
+
+
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+using namespace std;
+
+int solution(vector<string> want, vector<int> number, vector<string> discount) {
+    int answer = 0;
+    
+    for(int i=0; i<=discount.size()-10; i++){
+        bool signin = true;
+        
+        map<string, int> m;
+        for(int start=0+i; start<10+i; start++){
+            m[discount[start]] += 1;
+        }
+        
+        for(int j=0; j<want.size(); j++){
+            if(m.count(want[j]) == 0 || m[want[j]] != number[j]){
+                signin = false;
+                break;
+            }
+        }
+
+        if(signin){
             answer += 1;
         }
     }
