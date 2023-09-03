daf00d746e3cfb2b22c15dd2b12c17d543243925
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-09-03 20:20:08 +0900

    C++
    정답아님

---
+++ 프로그래머스/Level.2/할인 행사.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+using namespace std;
+
+// 슬라이딩 윈도우 알고리즘
+// number의 원소의 합은 10
+int solution(vector<string> want, vector<int> number, vector<string> discount) {
+    int answer = 0;
+    map<string, int> m;
+    for(int k=0; k<10; k++){
+        m[discount[k]] += 1;
+    }
+    
+    for(int i=0; i<=discount.size()-10; i++){
+        bool signin = true;
+        for(int j=0; j<want.size(); j++){
+            if(m[want[j]] != number[j]){
+                signin = false;
+                break;
+            }
+        }
+        
+        if(!signin && 10+i < discount.size()){
+            m[discount[i]] -= 1;
+            m[discount[i+10]] += 1;
+        }
+        else if(signin){
+            answer += 1;
+        }
+    }
+    // 원하는 제품을 모두 할인 받을 수 있는 회원등록 날짜의 총 일수
+    return answer;
+}
