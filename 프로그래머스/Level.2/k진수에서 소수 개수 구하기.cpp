d760312d7fbbda53970b0a07be6aafff880a1fa4
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-09-16 20:52:07 +0900

    C++

--- 프로그래머스/Level.2/k진수에서 소수 개수 구하기.cpp
+++ 프로그래머스/Level.2/k진수에서 소수 개수 구하기.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-bool sosu(long long n){
+bool prime(long long n){
     
     if(n<2) return false;
     
@@ -16,24 +16,26 @@ bool sosu(long long n){
 int solution(int n, int k) {
     int answer = 0;    
     string s = "";
+    
+    // 1. n 을 k 진수로 변환한다.
     while(n>0){
         s = to_string(n%k) + s;
         n /= k;
     }
+    s += "0";
     string temp = "";
-    
+   
     for(long long i=0; i<s.length(); i++){
+        // 2. k진수로 변환된 n을 0을 기준으로 나누어준다.
         if(s[i] == '0'){
-            if(temp != "" && sosu(stoll(temp))){
+            // 3. 나누어진 수가 소수일 경우 answer 에 1을 더해준다. 
+            if(temp != "" && prime(stoll(temp))){
                 answer += 1;
             }
             temp = "";
             continue;
         }
         temp += s[i];
-    }
-    
-    if(temp != "" && sosu(stoll(temp))) answer += 1;
-  
+    }  
     return answer;
 }
